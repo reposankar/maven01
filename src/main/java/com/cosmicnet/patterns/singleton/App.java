5f29fdc26ada0cbedf16dff3a57c1de557c84ec3
reposankar <45074975+reposankar@users.noreply.github.com>
2023-12-24 10:38:41 +0530

    Removing target

---
+++ src/main/java/com/cosmicnet/patterns/singleton/App.java
@@ -0,0 +1,14 @@
+package com.cosmicnet.patterns.singleton;
+
+
+
+class App {
+	public static void main(String[] args) {
+		AppConfig ac = AppConfig.getInstance();
+		System.out.println(ac);
+		AppConfig ac1 = new AppConfig();
+		System.out.println(ac1);
+		AppConfig ac2 = AppConfig.getInstance();
+		System.out.println(ac2);
+	}
+}
---
+++ src/main/java/com/cosmicnet/patterns/singleton/AppConfig.java
@@ -0,0 +1,67 @@
+package com.cosmicnet.patterns.singleton;
+
+import java.io.*;
+
+/**
+ * This is a singleton class.
+ * @author RepoSankar
+ *
+ */
+
+public class AppConfig implements Serializable, Cloneable {
+
+	/**
+	 * Singleton class to get an application configuration object.
+	 * Author : RepoSankar
+	 */
+	
+	public enum AppRAMType {INTEL, AMD, VEDANTA}
+	
+	private static final long serialVersionUID = -5075838354868831413L;
+	private static AppConfig self;
+	private int appRAM;
+	private AppRAMType appRAMType;
+	
+	public void setAppRAM(int ram) {
+		this.appRAM = ram;
+	}
+	public void setAppRAMType(AppRAMType ramType) {
+		this.appRAMType = ramType;
+	}
+	
+	public int getAppRAM() {
+		return appRAM;
+	}
+	
+	public AppRAMType getAppRAMType() {
+		return appRAMType;
+	}
+	
+	AppConfig() throws AssertionError{
+		if(self != null)
+			throw new AssertionError("Singleton class error.  Can not instanciate more than once!");
+			
+	}
+	
+	public static AppConfig getInstance() {
+		if(self == null) {
+			synchronized(AppConfig.class) {
+				if(self == null)
+					self = new AppConfig();
+				return self;
+			}
+		}
+		return self;
+	}
+	
+	Object readResolve() {
+		return self;
+	}
+	
+	@Override
+	protected Object clone() {
+		return self;
+	}
+	
+
+}
