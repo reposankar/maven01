42ce72bba07e797fd03f9cddc7a5b9f089c03587
reposankar <45074975+reposankar@users.noreply.github.com>
2024-01-01 21:20:43 +0530

    add clone object with primitives

---
+++ src/main/java/com/cosmicnet/effectivej/item14/Main.java
@@ -0,0 +1,22 @@
+package com.cosmicnet.effectivej.item14;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) {
+		PhoneNumber p1 = new PhoneNumber(23, 45, 103);
+		PhoneNumber p2 = new PhoneNumber(91, 96496, 75373);
+		PhoneNumber p3 = new PhoneNumber(1, 19, 6033);
+		PhoneNumber p4 = new PhoneNumber(44, 85, 2384);
+		PhoneNumber p5 = new PhoneNumber(5, 85, 527);
+		List<PhoneNumber> pl = Arrays.asList(p1, p2, p3, p4, p5);
+		System.out.println(pl);
+		List<Integer> ph1 = Arrays.asList(p1.hashCode(), p2.hashCode(), p3.hashCode(), p4.hashCode(), p5.hashCode());
+		System.out.println(ph1);
+		PhoneNumber ph2 = p1.clone();
+		System.out.println(ph2);
+		
+	}
+
+}
---
+++ src/main/java/com/cosmicnet/effectivej/item14/PhoneNumber.java
@@ -0,0 +1,62 @@
+package com.cosmicnet.effectivej.item14;
+
+import java.util.Objects;
+
+/**
+ * 
+ * @author Sankar M
+ * @version 1.0
+ * Description:
+ * 	This class is to demonstrate copy and comparable functionality.
+ *
+ */
+
+public class PhoneNumber implements Cloneable {
+	private int areaCode;
+	private int prefix;
+	private int lineNumber;
+	
+	public PhoneNumber(int areaCode, int prefix, int lineNumber) {
+		this.areaCode = areaCode;
+		this.prefix = prefix;
+		this.lineNumber = lineNumber;
+	}
+	
+	
+	public String toString() {
+		return String.format("\n%03d-%03d-%03d", areaCode, prefix, lineNumber);
+	}
+	
+	@Override
+	public int hashCode() {
+		int result = Objects.hash(areaCode);
+		result = 31 * result + Objects.hash(prefix);
+		result = 31 * result + Objects.hash(lineNumber);
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj)
+			return true;
+		else if (!(obj instanceof PhoneNumber))
+			return false;
+		else {
+			PhoneNumber pn = (PhoneNumber) obj;
+			return areaCode == pn.areaCode && prefix == pn.prefix && lineNumber == pn.lineNumber;
+		}
+	}
+	
+	@Override
+	public PhoneNumber clone() {
+		PhoneNumber result = null;
+		try {
+			result=  (PhoneNumber) super.clone();		
+		} catch(CloneNotSupportedException cloneex ) {
+			cloneex.printStackTrace();
+		}
+		return result;
+	}
+	
+	
+}
