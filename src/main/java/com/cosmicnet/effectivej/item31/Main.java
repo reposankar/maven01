7a88a5e87f7870d3d49e235e55a8496fb3cb6e7e
reposankar <45074975+reposankar@users.noreply.github.com>
2024-01-04 21:55:41 +0530

    modified to include simple wildcard generic for API release

--- src/main/java/com/cosmicnet/effectivej/item31/Main.java
+++ src/main/java/com/cosmicnet/effectivej/item31/Main.java
@@ -3,6 +3,8 @@ package com.cosmicnet.effectivej.item31;
 import java.security.InvalidParameterException;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
+import com.cosmicnet.effectivej.item07.Emp;
 
 
 /**
@@ -18,7 +20,7 @@ import java.util.ArrayList;
  *
  */
 public class Main {
-	
+
 	public static <T extends Comparable<? super T>> T max (List<? extends T> list) {
 		if (list.isEmpty())
 			throw new InvalidParameterException();
@@ -28,9 +30,25 @@ public class Main {
 				result = t;
 		return result;
 	}
-	
+
+	/*
+	public static  <Z> void swap(List<? extends Z> list, int i, int j) {
+		//list.set(j, list.set(i, list.get(j)));
+		swapHelper(list, i, j);
+	}
+	public static <T, E extends T> void swapHelper(List<E> list, int i, int j) {
+		list.set(j, list.set(i, list.get(j)));
+	}
+	*/
+	public static  void swap(List<?> list, int i, int j) {
+		//list.set(j, list.set(i, list.get(j)));
+		swapHelper(list, i, j);
+	}
+	public static <E> void swapHelper(List<E> list, int i, int j) {
+		list.set(j, list.set(i, list.get(j)));
+	}
 	public static void main(String[] args) {
-		
+
 		List<Cat> listOfCats= new ArrayList<>();
 		listOfCats.add(new Cat("Cat A"));
 		listOfCats.add(new Cat("Cat b"));
@@ -39,9 +57,8 @@ public class Main {
 		listOfCats.add(new Cat("Cat E"));
 		listOfCats.add(new Cat("Cat E"));
 		Cat max = max(listOfCats);
-		System.out.println(max);
+		//System.out.println(max);
 		System.out.println("-------------------------------");
-		
 		List<Feline> listOfFelines= new ArrayList<>();
 		listOfFelines.add(new Cat("A Cat"));
 		listOfFelines.add(new Cat("b Cat"));
@@ -50,8 +67,32 @@ public class Main {
 		listOfFelines.add(new Lion("E Lion"));
 		listOfFelines.add(new Cat("E Cat "));
 		Feline maxF = max(listOfFelines);
-		System.out.println(maxF);
+		//System.out.println(maxF);
+
 		System.out.println("-------------------------------");
+		for(Iterator<Feline> i = listOfFelines.iterator(); i.hasNext();) 
+			System.out.println(i.next());
+		System.out.println("-------------------------------");
+		swap(listOfFelines, 2, 3);
+		for(Iterator<Feline> i = listOfFelines.iterator(); i.hasNext();) 
+			System.out.println(i.next());
+
+		
+		List<Emp> listOfEmp= new ArrayList<>();
+		listOfEmp.add(new Emp("A Cat"));
+		listOfEmp.add(new Emp("b Cat"));
+		listOfEmp.add(new Emp("C Cat"));
+		listOfEmp.add(new Emp("d Lion"));
+		listOfEmp.add(new Emp("E Lion"));
+		listOfEmp.add(new Emp("E Cat "));
+		System.out.println("-------------------------------");
+		for(Iterator<Emp> i = listOfEmp.iterator(); i.hasNext();) 
+			System.out.println(i.next());
+		System.out.println("-------------------------------");
+		swap(listOfEmp, 2, 3);
+		for(Iterator<Emp> i = listOfEmp.iterator(); i.hasNext();) 
+			System.out.println(i.next());
+		
 	}
 
 }
